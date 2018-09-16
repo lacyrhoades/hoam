826843d05e122db42f2f0c0f56ae4c042d8b91a7
Lacy Rhoades <lacyrhoades@gmail.com>
2018-09-15 23:55:10 -0400

    Added onoff lib

--- index.js
+++ index.js
@@ -7,14 +7,41 @@ let twilio = new require('twilio')(twilioAuth.sid, twilioAuth.token)
 let dateFormat = require('dateformat')
 let now = new Date()
 
-numbers.forEach((number) => {
-    sendTo(number)
-})
+let Gpio = require('onoff').Gpio;
+let button = new Gpio(4, 'in', 'falling', {debounceTimeout: 10});
+let throttle = require('lodash/throttle')
+let timeout = 8 * 60 * 1000
+let options = {'trailing': false}
+
+let sendAll = (err, value) => {
+    if (err) {
+        throw err;
+    }
+
+    numbers.forEach((number) => {
+        sendTo(number)
+    })
+
+    console.log(value)
+}
+
+button.watch(
+    throttle(sendAll, timeout, options)
+    )
+
+console.log("Started doorbell watcher ...")
 
 function sendTo(number) {
+        return;
+
+
     twilio.messages.create({
-        body: "Someone rang the doorbell at " + house.address + " (" + dateFormat(now, "HH:MM m/d/yy") + ")",
+        body: "Doorbell at " + house.address + " (" + dateFormat(now, "HH:MM m/d/yy") + ")",
         to: number,
         from: twilioAuth.from
     })
 }
+
+process.on('SIGINT', () => {
+    button.unexport();
+});
